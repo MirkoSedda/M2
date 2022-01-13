8eb5627d77444fc679f3b7b0068fad70f11bf2aa
yourusername <kunay69@hotmail.it>
2022-01-13 14:42:41 +0100

    M2-D9 init

--- M2-D9/Homeworks/index.js
+++ M2-D9/Homeworks/index.js
@@ -1,1 +1,34 @@
-alert('im here')
+window.onload = function () {
+  addToggleButtons()
+
+  countAlbums()
+}
+
+function addToggleButtons() {
+  for (let section of document.querySelectorAll('section')) {
+    section.children[0].classList.add('collapse')
+    section.children[0].classList.add('show')
+
+    section.innerHTML =
+      `
+                        <button class='btn btn-outline-light toggle-btn' data-bs-toggle="collapse" data-bs-target="#${section.id} > div">
+                            Show/hide section
+                        </button>
+                    ` + section.innerHTML
+  }
+}
+
+function countAlbums() {
+  const albumN = document.querySelectorAll('img').length - 3
+
+  const span = document.createElement('span')
+
+  span.classList.add('text-center')
+  span.classList.add('d-inline-block')
+  span.classList.add('w-100')
+  span.classList.add('my-4')
+
+  span.innerHTML = 'P.S. This page contains ' + albumN + ' albums!'
+
+  document.querySelector('footer').appendChild(span)
+}
